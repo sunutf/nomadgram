b4b9084da2980be5a38e5323de882fadf4a3f91a
Daniel Seon <ggavy@Danielui-MacBook-Pro.local>
2018-01-13 18:34:12 +0900

    1-50 following/unfollowing User

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -33,34 +33,62 @@ class Feed(APIView):
 
 class LikeImage(APIView):
 
-    def get(self, request, image_id, format=None):
+    def post(self, request, image_id, format=None):
 
         user = request.user
 
         try:
             found_image = models.Image.objects.get(id=image_id)
         except models.Image.DoesNotExist:
-            return Response(status=404)
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
         try:
             preexisting_like = models.Like.objects.get(
                 creator=user,
                 image=found_image
             )
-            preexisting_like.delete()
 
-            return Response(status = 204)
+            return Response(status = status.HTTP_304_NOT_MODIFIED)
 
         except models.Like.DoesNotExist:
 
             new_like = models.Like.objects.create(
-            creator=user,
-            imafge=found_image
+                creator=user,
+                imafge=found_image
             )
 
             new_like.save()
 
             return Response(status = 200)
+
+
+class UnLikeImage(APIView):
+
+    def delete(self, request, image_id, format=None):
+        
+        user = request.user
+
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            preexisting_like = models.Like.objects.get(
+                creator=user,
+                image=found_image
+            )
+
+            preexisting_like.delete()
+
+            return Response(status = status.HTTP_204_NO_CONTENT)
+
+        except models.Like.DoesNotExist:
+
+            return Response(status = status.HTTP_304_NOT_MODIFIED)
+
+
+
 
 
 class CommentOnImage(APIView):
